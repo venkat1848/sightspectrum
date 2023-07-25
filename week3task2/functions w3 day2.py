fc1160d2c94155be4bc422d504acc5cd26ed22fc
SSLTP11171 <venkatasubbareddy.sightspectrum@gmail.com>
2023-07-25 05:57:49 -0700

    first commit

---
+++ week3task2/functions w3 day2.py
@@ -0,0 +1,103 @@
+##def addnumbers(n1=100,n2=1000):
+##    substract = n1-n2
+##    return substract
+##
+##result = addnumbers()
+##print(result)
+##
+##
+##def addnumbers(n1,n2=1):
+##    substract = n1-n2
+##    return substract
+##
+##result = addnumbers(14.345)
+##
+##
+##def functions(**kwargs):
+##    return kwargs
+##print(functions(a=10,b=20,c=30,d=40,e=34))    
+      
+##
+##
+##def functions(a,b,c):
+##    sum=a+b+c
+##    return sum
+##print(functions(12,34,78))
+##
+##def functions(a,b,c):
+##    return a,b,c
+##print(functions(56,20,23))
+##
+##def functions(b,a,c):
+##    sum=a+b-c
+##    return sum
+##print(functions(56,20,23))
+##
+##
+##def function(a,b,c):
+##    return c,c,a
+##print(function(22.5,36.2,24.7))
+##
+##
+##def functions (a,b,c):
+##    return a,b,c
+##print(functions(a=15,b=34,c=78))
+##        
+##
+##def functions (a,b,c):
+##    return b,c,a
+##print(functions(a=15,b=34,c=78))
+##
+##
+##
+##def functions(a,b,d,c=55):
+##    sum = b+a+c
+##    return sum
+##var = functions(1,2.5,6,7)
+##print(var)
+##if var == 10.5:
+##    print("sucess")
+##else:
+##    print("failures")
+##    
+##
+##def functions(a,b,d,c=55):
+##    sum = b+a+c
+##    return sum
+##print("subhareaddy") 
+##print(functions(1,2.5,6,7))
+##
+##def fun(a):
+##    print(a)
+##print("print")    
+##fun(10)      
+##
+##
+##
+##
+##def function(*args):
+##    return  args
+##print(function(1,22,24.5,36,5.11))
+##
+##
+##def functons(**kwargs):
+##    return kwargs
+##print(functions(a=10,b=25,c=42.4,d=1.3))
+
+def functions(a,b,c,d=55):
+    sum = b+a+c+d
+    return sum
+var = functions(1,2.5,6,47)
+print(var)
+if var >= 20:
+    print("sucess")
+else:
+    print("failures")
+
+
+
+
+
+
+
+
