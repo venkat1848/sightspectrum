49aafcbaeb8ad522e6cacf8c99f379d8c0cd4d7d
SSLTP11171 <venkatasubbareddy.sightspectrum@gmail.com>
2023-07-26 06:00:49 -0700

    first commit

---
+++ week3-task3/week -3 day -3  functions practise.py
@@ -0,0 +1,74 @@
+##def functions(x,y,z):
+##    return x,y,z
+##print(functions(45,67,89))
+##
+##
+##def functions(x,y,z):
+##    return z,x,y
+##print(functions(45,67,89))
+##
+##
+##def functions(x,y,z):
+##    return x,y,z
+##x=34
+##y=78
+##z=98
+##print(functions(y=12,x=45,z=34))
+
+##
+##def typeofnum(num):
+##    if num%2 == 0:
+##        def message():   
+##            print("you entered aneven number")
+##    else:
+##         def message():
+##            print("you entered  an odd number")
+##    message()
+    
+            
+##def fn(value):
+##    print(value)
+##    return
+
+
+##def typeofnum(x,y,a):
+##    return(y,y,a)
+##print(typeofnum(10,20,35))
+
+
+##def addnumbers(a=15,b=23,c=1253,d=73636):
+##    sum = a+b+c+d
+##    print("sum",sum)
+
+
+            
+##def display(firstname,lastname):
+##    print('firstname:',firstname)
+##    print('lastname:',lastname)
+
+    
+##
+##def findsum(*numbers):
+##    result=0
+##    for num in numbers:
+##        result = result + num
+##    print("sum = ",result)
+
+##
+##n= int(input().strip())
+##if n % 2 != 0:
+##    print("Weird")
+##elif n % 2 == 0 and n in range (2,6):
+##    print("Not Weird")
+##elif n % 2 == 0 and n in range(6,21) :
+##    print("Weird")
+##elif n % 2 == 0 and n > 20:
+##    print("Not Weird")
+
+
+         
+    
+
+
+
+
