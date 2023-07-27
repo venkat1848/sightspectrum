98d6842c511d272dfbdb86b754e9571a165dea8c
SSLTP11171 <venkatasubbareddy.sightspectrum@gmail.com>
2023-07-27 08:26:06 -0700

    first commit

---
+++ week3task4/w-3 day -4.py
@@ -0,0 +1,38 @@
+##def isleap(year):
+##    if year % 4 == 0:
+##        print("is year is true")
+##    else:
+##        print("is year is false")
+
+##x = lambda a: a +10
+##print((51))
+##
+##x = lambda a, b: a * b 
+##print(x(5, -10))
+##
+##
+##x = lambda a, b, c: a + b + c
+##print(x(55, 6.5, 244))
+##
+##x=lambda a,b,c,d:a+b-c+d
+##print(x(1,20,3,4))
+##
+##
+##greet = lambda : print('Hello World')
+##greet()
+
+
+##greetuser = lambda name : print('Hey there,', name)
+##greetuser('subbareddy')
+##
+##
+##max = lambda a,b,c : a if(a > c)  else c
+##print(max(1,8,10))
+##
+##
+##Max = lambda a, b : a if(a < b) else b
+## 
+##print(Max(10, 2))
+
+
+
