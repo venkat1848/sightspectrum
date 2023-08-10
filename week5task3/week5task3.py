cc281ceeceddf7ff519dc80dae50d4ba3fdbd5f3
SSLTP11171 <venkatasubbareddy.sightspectrum@gmail.com>
2023-08-09 21:34:25 -0700

    first commit

---
+++ week5task3/week5task3.py
@@ -0,0 +1,21 @@
+a = int(input("enter the value of a:"))
+b = int(input("enter the values of b:"))
+try:
+    c= a/b
+    print(c)
+except:
+    print("exception is overrised")
+else:
+    print("not excepted")
+finally:
+    print("end programm")
+
+
+
+try:
+  print("jai hind")
+except:
+  print("Something went wrong")
+else:
+  print("Nothing went wrong")
+ 
